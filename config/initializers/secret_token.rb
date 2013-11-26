fe73b0be8c0a115eff865628102391ae73d68519
snugforce <snugforce@mail.ru>
2013-11-26 09:36:01 +0400

    Zero point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01d8eda0a0ab99fceecc86b567654b7ff4f47e20b63c213c63406a3be9deabb25888e4e1a140d225ed2e9916248cd09b4c0370aad5771b3a007607ac38b35781'
